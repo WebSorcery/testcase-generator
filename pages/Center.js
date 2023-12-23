ac1c93c2617604a2d767fd980c3ae6762b8b873c
GAGAN RAGHAV <120806356+GAGANRAGHAV@users.noreply.github.com>
2023-12-23 09:48:33 +0530

    changes2

--- pages/Center.js
+++ pages/Center.js
@@ -1,8 +1,6 @@
 import React from "react";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
-import image from "./test.png";
 const inter = Inter({ subsets: ["latin"] });
 
 const Center = () => {
